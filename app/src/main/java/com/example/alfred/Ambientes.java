1a0025aa2468d435dce06d52ea4ceb648da4f269
Belén <belen.partenzi@gmail.com>
2019-12-10 20:52:41 -0300

    Initial commit

---
+++ app/src/main/java/com/example/alfred/Ambientes.java
@@ -0,0 +1,25 @@
+package com.example.alfred;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class Ambientes extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ambientes);
+
+        final EditText ip = findViewById(R.id.ip);
+        final ImageButton btnLuz = findViewById(R.id.btnLuz);
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/alfred/Login.java
@@ -0,0 +1,41 @@
+package com.example.alfred;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+
+        final Button btnIngresar = findViewById(R.id.btnIngresar);
+        final EditText usuario = findViewById(R.id.usuario);
+        final EditText password = findViewById(R.id.password);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Login.this, Ambientes.class);
+
+                Toast notificacion = Toast.makeText(getApplicationContext(), "Bienvenido a Alfred" , Toast.LENGTH_SHORT);
+                notificacion.show();
+
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+}
