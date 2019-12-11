1c8560dd09a988367d1b7f18981e42d15898f4ce
Belén <belen.partenzi@gmail.com>
2019-12-11 01:59:49 -0300

    funciona

--- app/src/main/java/com/example/alfred/Ambientes.java
+++ app/src/main/java/com/example/alfred/Ambientes.java
@@ -2,26 +2,101 @@ package com.example.alfred;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Ambientes extends AppCompatActivity {
+
+    EditText editTextAddress, editTextPort;
+    ImageButton buttonConnect;
+    TextView textViewState, textViewRx;
+
+    UdpClientHandler udpClientHandler;
+    UdpClientThread udpClientThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ambientes);
 
-        final EditText ip = findViewById(R.id.ip);
-        final ImageButton btnLuz = findViewById(R.id.btnLuz);
+        editTextAddress = (EditText) findViewById(R.id.address);
+        editTextPort = (EditText) findViewById(R.id.port);
+        buttonConnect = (ImageButton) findViewById(R.id.connect);
+        textViewState = (TextView)findViewById(R.id.state);
 
+        buttonConnect.setOnClickListener(buttonConnectOnClickListener);
 
+        udpClientHandler = new UdpClientHandler(this);
+    }
 
+    View.OnClickListener buttonConnectOnClickListener =
+            new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+
+                    udpClientThread = new UdpClientThread(
+                            editTextAddress.getText().toString(),
+                            Integer.parseInt(editTextPort.getText().toString()),
+                            udpClientHandler);
+                    udpClientThread.start();
+
+                    //buttonConnect.setEnabled(false);
+                }
+            };
+
+    private void updateState(String state){
+        textViewState.setText(state);
+    }
+
+    private void updateRxMsg(String rxmsg){
+        textViewRx.append(rxmsg + "\n");
+    }
+
+    private void clientEnd(){
+        udpClientThread = null;
+        textViewState.setText("clientEnd()");
+        buttonConnect.setEnabled(true);
 
     }
 
+    public static class UdpClientHandler extends Handler {
+        public static final int UPDATE_STATE = 0;
+        public static final int UPDATE_MSG = 1;
+        public static final int UPDATE_END = 2;
+        private Ambientes parent;
 
+        public UdpClientHandler(Ambientes parent) {
+            super();
+            this.parent = parent;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+
+            switch (msg.what){
+                case UPDATE_STATE:
+                    parent.updateState((String)msg.obj);
+                    break;
+                case UPDATE_MSG:
+                    parent.updateRxMsg((String)msg.obj);
+                    break;
+                case UPDATE_END:
+                    parent.clientEnd();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+
+        }
+    }
 }
---
+++ app/src/main/java/com/example/alfred/UdpClientThread.java
@@ -0,0 +1,69 @@
+package com.example.alfred;
+
+import android.os.Message;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class UdpClientThread extends Thread {
+
+    String dstAddress;
+    int dstPort;
+    private boolean running;
+    Ambientes.UdpClientHandler handler;
+
+    DatagramSocket socket;
+    InetAddress address;
+
+    public UdpClientThread(String addr, int port, Ambientes.UdpClientHandler handler) {
+        super();
+        dstAddress = addr;
+        dstPort = port;
+        this.handler = handler;
+    }
+
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    private void sendState(String state) {
+        handler.sendMessage(
+                Message.obtain(handler,
+                        Ambientes.UdpClientHandler.UPDATE_STATE, state));
+    }
+
+    @Override
+    public void run() {
+        sendState("connecting...");
+
+        running = true;
+
+        try {
+            socket = new DatagramSocket();
+            address = InetAddress.getByName(dstAddress);
+
+            // send request
+            byte[] buf = new byte[256];
+
+            DatagramPacket packet =
+                    new DatagramPacket(buf, buf.length, address, dstPort);
+            socket.send(packet);
+
+            sendState("connected");
+
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
